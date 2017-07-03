0ddad4d778f597526fe096f1cf74470edb8e55b5
ANDRZEJ SEMENIUK <andrzejsemeniuk@gmail.com>
2017-07-03 09:48:06 -0700

    Initial Commit

---
+++ productTweeper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  productTweeper
+//
+//  Created by andrzej semeniuk on 7/3/17.
+//  Copyright © 2017 Andrzej Semeniuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
