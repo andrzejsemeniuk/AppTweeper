d4443de39843773d9285776a141c790af20a2d00
andrzejsemeniuk <andrzejsemeniuk@gmail.com>
2017-07-23 10:20:01 -0700

    added radio wave and title animations

--- productTweeper/ViewControllerForScreenMain.swift
+++ productTweeper/ViewControllerForScreenMain.swift
@@ -34,11 +34,16 @@ class ViewControllerForScreenMain: UIViewController {
         stack.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
         stack.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
         
-        viewForImageTop.translatesAutoresizingMaskIntoConstraints=false
-        viewForImageTop.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: -self.view.frame.height/3.0).isActive=true
-//        viewForImageTop.bottomAnchor.constraint(equalTo: stack.topAnchor).isActive = true
-//        viewForImageTop.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
-        viewForImageTop.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive=true
+        if false {
+            viewForImageTop.translatesAutoresizingMaskIntoConstraints=false
+            viewForImageTop.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: -self.view.frame.height/3.0).isActive=true
+            viewForImageTop.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive=true
+        }
+        else {
+            viewForImageTop.center.x = self.view.center.x
+            viewForImageTop.center.y = self.view.frame.top/5.0
+            viewForImageTop.translatesAutoresizingMaskIntoConstraints=true
+        }
 
 //        viewForImageTop.align(above:viewForMenu, constant:16)
 //        viewForImageTop.alignCenterXWithParent(constant:0)
@@ -82,6 +87,10 @@ class ViewControllerForScreenMain: UIViewController {
 
         // "background gradient"
 
+        let gradientView = UIView(frame:self.view.frame)
+        
+        self.view.insertSubview(gradientView, at: 0)
+        
         gradient = CAGradientRadialLayer()
         
         gradient.bounds = self.view.bounds
@@ -94,7 +103,25 @@ class ViewControllerForScreenMain: UIViewController {
         //        gradient.frame.origin = CGPoint(x:0,y:0)
         gradient.anchorPoint = CGPoint(x:0,y:0)
         
-        self.view.layer.insertSublayer(gradient, at: 0)
+        gradientView.layer.insertSublayer(gradient, at: 0)
+        
+        // motion effect
+        
+        let margin:CGFloat = 50
+        
+        let motionX = UIInterpolatingMotionEffect.init(keyPath: "layer.transform.translation.x", type: .tiltAlongHorizontalAxis)
+        motionX.minimumRelativeValue = -margin
+        motionX.maximumRelativeValue = +margin
+        
+        let motionY = UIInterpolatingMotionEffect.init(keyPath: "layer.transform.translation.y", type: .tiltAlongVerticalAxis)
+        motionY.minimumRelativeValue = -margin
+        motionY.maximumRelativeValue = +margin
+        
+        let motionEffect = UIMotionEffectGroup()
+        motionEffect.motionEffects = [motionX,motionY]
+        
+        self.viewForImageTop.addMotionEffect(motionEffect)
+        
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -106,6 +133,62 @@ class ViewControllerForScreenMain: UIViewController {
             button.isSelected = false
             button.isEnabled = true
         }
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // radio effect
+        
+        let radius = self.view.frame.diagonal
+        let diameter = radius * 2
+        let count = 12
+        let duration = 24.0
+        for animation in 0..<count
+        {
+            let radioWaveView = UIView(frame:CGRect(origin : self.viewForImageTop.center - radius,
+                                                    size   : CGSize(side:diameter)))
+            
+            self.view.insertSubview(radioWaveView, at: 1)
+            
+            radioWaveView.backgroundColor = .clear
+            
+            let circle = UIBezierPath()
+            circle.addArc(withCenter : CGPoint(xy:radius),
+                          radius     : radius,
+                          startAngle : 0,
+                          endAngle   : .pi * 2,
+                          clockwise  : false)
+            circle.close()
+            
+            let layer = CAShapeLayer()
+            
+            layer.strokeColor   = UIColor.white.cgColor
+            layer.fillColor     = UIColor(hsba:[0,0,0,0.0]).cgColor
+            layer.lineWidth     = 30
+            layer.path          = circle.cgPath
+            
+            radioWaveView.layer.addSublayer(layer)
+            
+            radioWaveView.transform = CGAffineTransform.init(scaleX: 0.001, y: 0.001)
+            radioWaveView.alpha = 0.1
+            
+            let delay = Double(animation)/Double(count)*duration
+            UIView.animate(withDuration: TimeInterval(duration), delay: delay, options: [.repeat, .curveLinear], animations: {
+                radioWaveView.transform = CGAffineTransform(scaleX: 1, y: 1)
+                radioWaveView.alpha = 0.02
+            }) { finished in
+                print("finished=\(finished) @ \(delay)")
+            }
+
+        }
+        
+        UIView.animate(withDuration: 1, delay: 0, options: [.repeat, .autoreverse], animations: {
+            self.viewForImageTop.transform = CGAffineTransform.init(scaleX: 1.04, y: 1.1)
+        })
+        
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -118,7 +201,8 @@ class ViewControllerForScreenMain: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
     @IBAction func tapOnButton              (_ sender:UIControl!) {
         sender.isSelected=true
         for button in buttons {
@@ -136,3 +220,49 @@ class ViewControllerForScreenMain: UIViewController {
     }
 
 }
+
+open class CADrawLayer : CALayer {
+
+    public typealias Drawing = (CALayer, CGContext)->Void
+    
+    var drawings:[Drawing] = []
+    
+    override public init() {
+        super.init()
+        self.needsDisplayOnBoundsChange=true
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public func add(drawing:@escaping Drawing) {
+        drawings.append(drawing)
+    }
+    
+    override open func draw(in context: CGContext) {
+        for drawing in Array<Drawing>(drawings) {
+            drawing(self, context)
+        }
+    }
+
+}
+
+open class UIDrawView : UIView {
+    
+    public typealias Drawing = (UIView,CGContext,CGRect)->Void
+    
+    var drawings:[Drawing] = []
+    
+    public func add(drawing:@escaping Drawing) {
+        drawings.append(drawing)
+    }
+
+    override open func draw(_ rect: CGRect) {
+        let context = UIGraphicsGetCurrentContext()!
+        for drawing in Array<Drawing>(drawings) {
+            drawing(self, context, rect)
+        }
+    }
+}
+
